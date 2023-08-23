2b2d5766ac032b87cb640114b04493e1e07621b5
DanysOrtiz <111403958+DanysOrtiz@users.noreply.github.com>
2023-08-23 04:44:48 -0500

    Nuevo archivo Enlaces Agosto 2023

---
+++ data/Enlaces_full.js
@@ -0,0 +1,65 @@
+var Contactos = {"type":"FeatureCollection",
+"features":[
+    {"type":"Feature","properties":{"COD_DEPART":"11","DEPARTAMEN":"BOGOTA","Nombre_Departamento":"BOGOTA. D.C.","Nombres":"Diana Murillo","Correo_email":"socioambientaltej@minenergia.gov.co","EnlaceDepartamento":"Tr. Energetica Justa","NumeroTel":"3103080839","Sector":"TEJ"},
+    "geometry":{"type":"Point","coordinates":[-74.10083240136885,4.734576699262354]}},
+    {"type":"Feature","properties":{"COD_DEPART":"11","DEPARTAMEN":"BOGOTA","Nombre_Departamento":"BOGOTA. D.C.","Nombres":"Mauricio Velasquez","Correo_email":"andreymauricio.velasquez@gmail.com  o avelasquezg@fucolde.org","EnlaceDepartamento":"Equipo Jurídico PQRSD","NumeroTel":"3138585524","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-74.16083240136885,4.584576699262354]}},
+    {"type":"Feature","properties":{"COD_DEPART":"11","DEPARTAMEN":"BOGOTA","Nombre_Departamento":"BOGOTA. D.C.","Nombres":"Angela Carmenza Gallego","Correo_email":"carmenag69@gmail.com","EnlaceDepartamento":"Nacional","NumeroTel":"3102036975","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-74.07083240136885,4.6476699262354]}},
+    {"type":"Feature","properties":{"COD_DEPART":"11","DEPARTAMEN":"BOGOTA","Nombre_Departamento":"BOGOTA. D.C.","Nombres":"Mónica Ucros Escallón","Correo_email":"mucros@fucolde.org","EnlaceDepartamento":"Nacional","NumeroTel":"3153893409","Sector":"Mineria"},
+    "geometry":{"type":"Point","coordinates":[-74.08083240136885,4.574576699262354]}},
+    {"type":"Feature","properties":{"COD_DEPART":"13","DEPARTAMEN":"BOLÍVAR","Nombre_Departamento":"BOLIVAR","Nombres":"Karen Irina Galván Primera","Correo_email":"kigp04@hotmail.com","EnlaceDepartamento":"Caribe","NumeroTel":"3127029372","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-74.50500731231047,8.74115871585547]}},
+    {"type":"Feature","properties":{"COD_DEPART":"15","DEPARTAMEN":"BOYACÁ","Cod_4":15.0,"Nombre_Departamento":"BOYACA","Nombres":"Angela Pedraza","Correo_email":null,"EnlaceDepartamento":"Boyacá","NumeroTel":null,"Sector":"Energia"},
+    "geometry":{"type":"Point","coordinates":[-73.10213637759405,5.77669056907321]}},
+    {"type":"Feature","properties":{"COD_DEPART":"15","DEPARTAMEN":"BOYACÁ","Nombre_Departamento":"BOYACA","Nombres":"Pedro Felipe Orjuela Benavides","Correo_email":"pforjuelab@gmail.com","EnlaceDepartamento":"Boyacá","NumeroTel":"3115384624","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-73.15213637759405,5.07669056907321]}},
+    {"type":"Feature","properties":{"COD_DEPART":"18","DEPARTAMEN":"CAQUETÁ","Nombre_Departamento":"CAQUETA","Nombres":"Orfilia Gonzalez garcia","Correo_email":"ogonzalezg@fucolde.org","EnlaceDepartamento":"Caquetá","NumeroTel":"3107707026","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-73.9592677969858,0.7984805797804451]}},
+    {"type":"Feature","properties":{"COD_DEPART":"20","DEPARTAMEN":"CESAR","Nombre_Departamento":"CESAR","Nombres":"Anyela Marcela Martínez pitre","Correo_email":"anyelamartinez11@outlook.es","EnlaceDepartamento":"Cesar","NumeroTel":"3124612473","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-73.51801754358887,9.534831623922694]}},
+    {"type":"Feature","properties":{"COD_DEPART":"20","DEPARTAMEN":"CESAR","Nombre_Departamento":"CESAR","Nombres":"María Fabeli Ávila Gasca","Correo_email":"mavilag@fucolde.org","EnlaceDepartamento":"Cesar","NumeroTel":"3218024314","Sector":"Mineria"},
+    "geometry":{"type":"Point","coordinates":[-73.51801754358887,9.334831623922694]}},
+    {"type":"Feature","properties":{"COD_DEPART":"25","DEPARTAMEN":"CUNDINAMARCA","Nombre_Departamento":"CUNDINAMARCA","Nombres":"Sandra Patricia Sanabria Hernández","Correo_email":"sanabriasandra27@gmail.com","EnlaceDepartamento":"Cundinamarca","NumeroTel":"3006046646","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-74.10743990462825,5.054041261099757]}},
+    {"type":"Feature","properties":{"COD_DEPART":"41","DEPARTAMEN":"HUILA","Nombre_Departamento":"HUILA","Nombres":"Liliana Quimbaya","Correo_email":"mquimbaya@hotmail.com","EnlaceDepartamento":"Huila","NumeroTel":"3102614532","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-75.58466341580565,2.5699134442916029]}},
+    {"type":"Feature","properties":{"COD_DEPART":"44","DEPARTAMEN":"LA GUAJIRA","Nombre_Departamento":"LA GUAJIRA","Nombres":"Miguel Alfono Ochoa","Correo_email":null,"EnlaceDepartamento":"Guajira","NumeroTel":"3166239254","Sector":"Mineria"},
+    "geometry":{"type":"Point","coordinates":[-72.43141628521716,11.476652191394303]}},
+    {"type":"Feature","properties":{"COD_DEPART":"5","DEPARTAMEN":"ANTIOQUIA","Nombre_Departamento":"ANTIOQUIA","Nombres":"Adriana Carmona","Correo_email":"ajcarmona@minenergia.gov","EnlaceDepartamento":null,"NumeroTel":"314 6157341","Sector":"Mineria"},
+    "geometry":{"type":"Point","coordinates":[-75.56762479266576,6.923498193242816]}},
+    {"type":"Feature","properties":{"COD_DEPART":"50","DEPARTAMEN":"META","Nombre_Departamento":"META","Nombres":"Jorge Alexander Pedroza Romero","Correo_email":"jpedrozar02@hotmail.com","EnlaceDepartamento":"META","NumeroTel":"3124642035","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-72.95678941768419,3.3460124140694718]}},
+    {"type":"Feature","properties":{"COD_DEPART":"50","DEPARTAMEN":"META","Nombre_Departamento":"META","Nombres":"Andres Felipe Chaverra Padilla","Correo_email":"achaverr@fucolde.org","EnlaceDepartamento":"Meta","NumeroTel":"3153831813","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-72.85678941768419,3.5460124140694718]}},
+    {"type":"Feature","properties":{"COD_DEPART":"54","DEPARTAMEN":"NORTE DE SANTANDER","Nombre_Departamento":"NORTE DE SANTANDER","Nombres":"Paola Cristina Jaimes Jáuregui","Correo_email":"paolajaimes14@gmail.com","EnlaceDepartamento":"Norte de Santander","NumeroTel":"3045418133","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-72.8807406458435,8.094608292967444]}},
+    {"type":"Feature","properties":{"COD_DEPART":"54","DEPARTAMEN":"NORTE DE SANTANDER","Nombre_Departamento":"NORTE DE SANTANDER","Nombres":"Yan Carlos Becerra Pineda","Correo_email":null,"EnlaceDepartamento":"Norte de Santander","NumeroTel":null,"Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-72.8807406458435,8.074608292967444]}},
+    {"type":"Feature","properties":{"COD_DEPART":"73","DEPARTAMEN":"TOLIMA","Nombre_Departamento":"TOLIMA","Nombres":"Mauricio Eliecer Giraldo Mejía","Correo_email":"mgiraldom@fucolde.org","EnlaceDepartamento":"Magdalena Medio - Bogotá","NumeroTel":"3135877492","Sector":"Mineria"},
+    "geometry":{"type":"Point","coordinates":[-75.25609616755426,4.034323190517338]}},
+    {"type":"Feature","properties":{"COD_DEPART":"73","DEPARTAMEN":"TOLIMA","Nombre_Departamento":"TOLIMA","Nombres":"Maria del Pilar Sánchez Saavedra","Correo_email":"msanchezs@fucolde.org","EnlaceDepartamento":"Tolima","NumeroTel":"3134040529","Sector":"Mineria"},
+    "geometry":{"type":"Point","coordinates":[-75.45609616755426,4.234323190517338]}},
+    {"type":"Feature","properties":{"COD_DEPART":"81","DEPARTAMEN":"ARAUCA","Nombre_Departamento":"ARAUCA","Nombres":"Bernardo Jose Ariza","Correo_email":"bariza@fucolde.org","EnlaceDepartamento":"Arauca","NumeroTel":"3102552829","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-70.76756941045683,6.670151138556432]}},
+    {"type":"Feature","properties":{"COD_DEPART":"81","DEPARTAMEN":"ARAUCA","Nombre_Departamento":"ARAUCA","Nombres":"Robinson Barragán Moreno","Correo_email":"ing_robinson@hotmail.com","EnlaceDepartamento":"Arauca","NumeroTel":"3103831448","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-70.96756941045683,6.570151138556432]}},
+    {"type":"Feature","properties":{"COD_DEPART":"85","DEPARTAMEN":"CASANARE","Nombre_Departamento":"CASANARE","Nombres":"ShirleyPatricia Moreno","Correo_email":null,"EnlaceDepartamento":"Casanare","NumeroTel":null,"Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-71.60315072488851,5.404766761205079]}},
+    {"type":"Feature","properties":{"COD_DEPART":"85","DEPARTAMEN":"CASANARE","Nombre_Departamento":"CASANARE","Nombres":"Jonathan Rodríguez Colina","Correo_email":"jonrodriguezco@gmail.com","EnlaceDepartamento":"Casanare","NumeroTel":"3162459962","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-71.58315072488851,5.704766761205079]}},
+    {"type":"Feature","properties":{"COD_DEPART":"86","DEPARTAMEN":"PUTUMAYO","Nombre_Departamento":"PUTUMAYO","Nombres":"Wilfredo Alfonso Ruales Guevara","Correo_email":"wilo127@hotmail.com","EnlaceDepartamento":"PUTUMAYO","NumeroTel":"3102932494","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-75.8563474286822,0.4521189585893326]}},
+    {"type":"Feature","properties":{"COD_DEPART":"91","DEPARTAMEN":"AMAZONAS","Nombre_Departamento":"AMAZONAS","Nombres":"Hector Dussan Rojas","Correo_email":"impordehsas@gmail.com  hdussan@fucolde.org","EnlaceDepartamento":"Amazonas","NumeroTel":"3115897656","Sector":"Hidrocarburos"},
+    "geometry":{"type":"Point","coordinates":[-71.50267142728716,-1.5461490953128435]}}
+]}
+
+L.geoJSON(Contactos,{
+    style: {
+        color:"green"
+    }
+}).bindPopup(function(layer){
+    numero = layer.feature.properties.NumeroTel
+    link = "<a href='https://wa.me/57"+ numero + "'>Whatsapp Link<a/>"
+    return "<strong>NOMBRE: </strong>" + layer.feature.properties.Nombres + "<br/>" + "<strong>TELEFONO: </strong>" + layer.feature.properties.NumeroTel + "<br/>" + "<strong>CORREO: </strong>" + layer.feature.properties.Correo_email + "<br/>" + link
+}).addTo(mapa)
